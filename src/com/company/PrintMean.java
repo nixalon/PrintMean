7fb887ab3848b105902bedd4387a122b141f81e9
DESKTOP-UEFB4MF\Alexandra <alexandra.nicklasson@student.kyh.se>
2020-09-10 13:26:09 +0200

    Added comment

--- src/com/company/PrintMean.java
+++ src/com/company/PrintMean.java
@@ -4,6 +4,7 @@ public class PrintMean {
 
     public static void main(String[] args) {
         //Heltalsdivision, skriver ut int istället för double
+        //Test
 
         System.out.println("Mean of 1, 1, 3, 3: " + meanCalc(1, 1, 3, 3));
         System.out.println("Mean of 1, 2, 3, 4: " + meanCalc(1, 2, 3, 4));
